074e5d18bcb97a1ec8c55df0c548d0264cea43e3
mungoP <mungoshearer@gmail.com>
2026-02-23 17:49:21 -0500

    fixing up

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,11 +3,7 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
-<<<<<<< HEAD
-This is to simulate a change made on a robot: robot_speed = 8 # m/s
-=======
 This is to simulate a change made on a robot: robot_speed = 3 # m/s
->>>>>>> a32b05b679e101110d6385eab19cb3f4b2124362
 """
 
 
